048b5c7d65592712935c9078e1aa5394762fe21a
Nathaniel Hindman <nathaniel.hindman@gmail.com>
2013-09-16 10:06:54 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NateApp::Application.config.secret_key_base = '4ccb5ded08e2d6aff49f4841ef465ece51cb0f3daa4677cb97906035691ceaed98235249fc0515be19ccd16e96c46fd9bbc46e0c7ced117dde869205d0033ba4'
