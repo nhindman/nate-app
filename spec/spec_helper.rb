087cbb807d5d90bd09c80f09d4404374bfe8d430
Nathaniel Hindman <nathaniel.hindman@gmail.com>
2013-09-21 21:43:09 -0400

    Finish layout and routes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,14 @@ Spork.prefork do
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
-  # Requires supporting ruby files with custom matchers and macros, etc, # in spec/support/ and its subdirectories. Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  # Requires supporting ruby files with custom matchers and macros, etc, 
+  # in spec/support/ and its subdirectories. 
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
 # Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line. ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+# If you are not using ActiveRecord, you can remove this line. 
+ActiveRecord::Migration.check_pending! if defined? (ActiveRecord::Migration)
+
 RSpec.configure do |config|
 # ## Mock Framework
 #
@@ -17,7 +22,9 @@ RSpec.configure do |config|
     # config.mock_with :rr
     # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
-# If you're not using ActiveRecord, or you'd prefer not to run each of your # examples within a transaction, remove the following line or assign false # instead of true.
+# If you're not using ActiveRecord, or you'd prefer not to run each of your 
+# examples within a transaction, remove the following line or assign false 
+# instead of true.
 config.use_transactional_fixtures = true
 # If true, the base class of anonymous controllers will be inferred
 # automatically. This will be the default behavior in future versions of # rspec-rails.
@@ -27,7 +34,8 @@ config.infer_base_class_for_anonymous_controllers = false
 # --seed 1234
 config.order = "random"
 config.include Capybara::DSL
-end end
+end 
+end
 Spork.each_run do
 # This code will be run each time you run your specs.
 end
